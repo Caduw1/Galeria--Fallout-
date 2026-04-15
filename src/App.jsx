985cf4470904c4f26068aeafd44146c405db6111
matheusk914 <matheusk914@gmail.com>
2026-04-15 20:21:35 -0300

    Atualização pasta dados

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Card from './Components/Card.jsx';
+import ItemCard from './Components/ItemCard.jsx';
+import Humanos from './Dados/Humanos.jsx';
+import Mutantes from './Dados/Mutantes.jsx';
+import Itens from "./Dados/Itens.jsx";
+
+const categorias = ["Humanos", "Mutantes", "Itens"];
+const dados = [...Humanos, ...Mutantes, ...Itens];
+
+function App() {
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("Humanos");
+
+  const dadosFiltrados = dados.filter(
+    (item) => item.categoria === categoriaSelecionada
+  );
+
+  return (
+   <div className="min-h-screen bg-gradient-to-b from-green-900 via-green-800 to-green-700 relative">
+  <div className="absolute inset-0 bg-green-950/30"></div>
+
+      <div className="relative z-10">
+        <h1 className="text-4xl font-bold text-center py-6 text-yellow-100 drop-shadow-lg">
+          Fallout - Wasteland
+        </h1>
+
+        <div className="flex justify-center mb-6">
+          <select
+            className="px-4 py-2 rounded-lg bg-yellow-800 text-yellow-100 font-semibold shadow-lg"
+            value={categoriaSelecionada}
+            onChange={(e) => setCategoriaSelecionada(e.target.value)}
+          >
+            {categorias.map((cat) => (
+              <option key={cat} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="p-5 flex flex-wrap gap-6 justify-center">
+          {dadosFiltrados.map((item) =>
+            item.categoria === "Itens" ? (
+              <ItemCard
+                key={item.id}
+                nome={item.nome}
+                descricao={item.descricao}
+                imagem={item.imagem}
+              />
+            ) : (
+              <Card
+                key={item.id}
+                nome={item.nome}
+                vida={item.vida}
+                categoria={item.categoria}
+                descricao={item.descricao}
+                imagem={item.imagem}
+              />
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
