3bfd24351e404cf4dbc06ac78be3d75e346ef6af
matheusk914 <matheusk914@gmail.com>
2026-04-15 22:05:58 -0300

    Correção de bug

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,4 @@
 function Card({ nome, vida, categoria, descricao, imagem }) {
-<<<<<<< HEAD
   return (
     <div className="w-[220px] p-4 rounded-lg bg-gray-900 text-gray-200 border border-gray-600 flex flex-col items-center gap-2 hover:scale-105 transition">
 
@@ -15,45 +14,10 @@ function Card({ nome, vida, categoria, descricao, imagem }) {
 
       <p className="text-sm">{descricao}</p>
 
-      <p className="font-bold">HP: {vida}</p>
-
-=======
-  const isItem = categoria === "Itens";
-
-  return (
-    <div
-      className={`w-[220px] p-4 rounded-lg flex flex-col items-center text-center gap-2
-      transition-transform duration-200 hover:scale-105
-      ${
-        isItem
-          ? "border border-green-400 bg-green-950 text-green-400 shadow-[0_0_10px_#00ff99]"
-          : "border border-gray-600 bg-gray-900 text-gray-200"
-      }`}
-    >
-      <img
-        src={imagem}
-        alt={nome}
-        className="w-full h-[160px] object-contain bg-gray-800 rounded"
-      />
-
-      <h3 className="text-lg font-semibold">
-        {nome}
-      </h3>
-
-      <p className="text-xs text-gray-400">
-        <strong>Categoria:</strong> {categoria || "Desconhecida"}
-      </p>
-
-      <p className="text-sm">
-        {descricao}
-      </p>
-
-      {!isItem && vida && (
-        <p className="text-sm font-bold">
-          HP: {vida}
-        </p>
+      {vida && (
+        <p className="font-bold">HP: {vida}</p>
       )}
->>>>>>> 0f074875ca581961156dd9262431391e2e5fbd8c
+
     </div>
   );
 }
--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -1,34 +1,17 @@
 function ItemCard({ nome, descricao, imagem }) {
   return (
-<<<<<<< HEAD
     <div className="w-[220px] p-4 rounded-lg bg-green-950 text-green-400 border border-green-400 flex flex-col items-center gap-2 hover:scale-105 transition">
 
-=======
-    <div className="w-[220px] p-4 rounded-lg flex flex-col items-center text-center gap-2
-      border border-green-400 bg-green-950 text-green-400 shadow-[0_0_10px_#00ff99]
-      transition-transform duration-200 hover:scale-105"
-    >
->>>>>>> 0f074875ca581961156dd9262431391e2e5fbd8c
       <img
         src={imagem}
         alt={nome}
         className="w-full h-[140px] object-contain bg-black rounded"
       />
 
-<<<<<<< HEAD
       <h3 className="text-lg font-bold">{nome}</h3>
 
       <p className="text-sm">{descricao}</p>
 
-=======
-      <h3 className="text-lg font-semibold">
-        {nome}
-      </h3>
-
-      <p className="text-sm">
-        {descricao}
-      </p>
->>>>>>> 0f074875ca581961156dd9262431391e2e5fbd8c
     </div>
   );
 }
